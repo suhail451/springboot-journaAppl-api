d4864c2b11a504026276a754f98a6973425866bc
Suhail Ahmed <arainsuhail451@gmail.com>
2025-05-04 13:05:50 +0500

    Add files via upload

---
+++ First/First/src/main/java/com/learning/First/journalApp/controller/healthcheck/Entity.java
@@ -0,0 +1,31 @@
+package com.learning.First.journalApp.controller.healthcheck;
+
+public class Entity {
+    private int id;
+    private String name;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
